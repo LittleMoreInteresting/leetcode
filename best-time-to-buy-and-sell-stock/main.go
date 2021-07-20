c072b4a2b8b7f89e84468296be32f5be0873da07
shr26207 <sunhaoran@offcn.com>
2021-07-20 15:02:04 +0800

    best-time-to-buy-and-sell-stock

--- best-time-to-buy-and-sell-stock/main.go
+++ best-time-to-buy-and-sell-stock/main.go
@@ -1,7 +1,9 @@
 package main
 
+import "fmt"
+
 /**
-给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
 
 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
 
@@ -20,5 +22,32 @@ package main
 */
 
 func main() {
+	prices := []int{1, 2}
+	//prices := []int{7,1,5,3,6,4}
+	sell := bestSell(prices)
+	fmt.Println(sell)
+}
 
+func bestSell(prices []int) int {
+	var low, maxProfit int
+	size := len(prices)
+	if size <= 1 {
+		return 0
+	}
+	low = prices[0]
+	for i := 0; i < size; i++ {
+		if low > prices[i] {
+			low = prices[i]
+		}
+		maxProfit = max(maxProfit, prices[i]-low)
+		fmt.Printf("low:%v;maxProfit:%v\n", low, maxProfit)
+	}
+	return maxProfit
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
