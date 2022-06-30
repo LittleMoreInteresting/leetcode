8aa292b44611cc2dcbad9cf140ad3944dcac496a
shr26207 <sunhaoran@offcn.com>
2022-06-30 15:04:40 +0800

    rwmap 2022.06.28

---
+++ algorithm/rwmap/rwmap_test.go
@@ -0,0 +1,21 @@
+package rwmap
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMap(t *testing.T) {
+	m := NewRWMap[string, int](10)
+	m.Set("key1", 10)
+	v1, _ := m.Get("key1")
+	fmt.Println(v1)
+
+}
+
+func TestFnv(t *testing.T) {
+	h1 := fnv32("abc")
+	h2 := fnv320("abc")
+	fmt.Println(h1 & 63)
+	fmt.Println(h2 & 63)
+}
