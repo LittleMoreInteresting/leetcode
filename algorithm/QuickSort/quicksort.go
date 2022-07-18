1c36cf7b1a7f6752dea5a0b6834cf4bb6ac557f6
shr26207 <sunhaoran@offcn.com>
2022-07-18 15:48:13 +0800

    QuickSort

---
+++ algorithm/QuickSort/quicksort.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func Partition(nums *[]int, low, height int) int {
+	i, j, pivot := low, height, (*nums)[low]
+	for i < j {
+		for i < j && (*nums)[j] > pivot {
+			j--
+		}
+		if i < j {
+			Swap(nums, i, j)
+			i++
+		}
+		for i < j && (*nums)[i] < pivot {
+			i++
+		}
+		if i < j {
+			Swap(nums, i, j)
+			j--
+		}
+	}
+	return i
+}
+
+func Swap(nums *[]int, i, j int) {
+	(*nums)[i], (*nums)[j] = (*nums)[j], (*nums)[i]
+}
+
+func QuickSort(nums *[]int, low, height int) {
+	if low < height {
+		mid := Partition(nums, low, height)
+		QuickSort(nums, low, mid-1)
+		QuickSort(nums, mid+1, height)
+	}
+}
+func main() {
+	nums := []int{2, 4, 2, 5, 762, 141}
+	QuickSort(&nums, 0, len(nums)-1)
+	fmt.Println(nums)
+	nums = []int{}
+	QuickSort(&nums, 0, len(nums)-1)
+	fmt.Println(nums)
+	nums = []int{5, 2}
+	QuickSort(&nums, 0, len(nums)-1)
+	fmt.Println(nums)
+}
