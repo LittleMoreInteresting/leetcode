c14425cdf546c74fad6e2d588fbc5dfca702f563
shr26207 <sunhaoran@offcn.com>
2022-08-01 17:01:13 +0800

    p5 动态规划解法

--- labuladong/array/prob5/longestPalindrome.go
+++ labuladong/array/prob5/longestPalindrome.go
@@ -22,3 +22,47 @@ func palindrome(s string, l, r int) string {
 	}
 	return s[l+1 : r]
 }
+
+// 动态规划解法
+
+func longestPalindrome_DP(s string) string {
+	//1 定义DP数组  dp[i][j] 代表子串 s[i]~s[j] 是否为回文字符串
+	length := len(s)
+	if length == 0 {
+		return ""
+	}
+	dp := initDp(length)
+	//2 定义初始状态
+	start, longest := 0, 1
+	for i := 0; i < length; i++ {
+		dp[i][i] = true
+		if i < length-1 {
+			dp[i][i+1] = s[i] == s[i+1]
+			if dp[i][i+1] {
+				start = i
+				longest = 2
+			}
+		}
+	}
+
+	//3 状态转移方程 dp[i][j] = dp[i+1][j-1] & s[i] == s[j]
+	for i := length - 1; i >= 0; i-- {
+		for j := i + 2; j < length; j++ {
+			dp[i][j] = dp[i+1][j-1] && s[i] == s[j]
+			if dp[i][j] && j-i+1 > longest {
+				start = i
+				longest = j - i + 1
+			}
+		}
+	}
+	return s[start : start+longest]
+}
+
+func initDp(len int) [][]bool {
+	dp := make([][]bool, len)
+	dpTemp := make([]bool, len*len)
+	for i := 0; i < len; i++ {
+		dp[i], dpTemp = dpTemp[0:len], dpTemp[len:]
+	}
+	return dp
+}
