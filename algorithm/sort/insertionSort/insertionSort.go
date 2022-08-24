e0c830b4cbc17fa55aab03faaf3cdf38839cae48
shr26207 <sunhaoran@offcn.com>
2022-08-24 15:20:44 +0800

    排序

---
+++ algorithm/sort/insertionSort/insertionSort.go
@@ -0,0 +1,87 @@
+package main
+
+import "fmt"
+
+func insertionSort(nums []int, n int) {
+	if n <= 1 {
+		return
+	}
+	for i := 1; i < n; i++ {
+		value := nums[i]
+		j := i - 1
+		for ; j >= 0; j-- {
+			if nums[j] > value {
+				nums[j+1] = nums[j]
+			} else {
+				break
+			}
+		}
+		nums[j+1] = value
+	}
+}
+
+//冒泡排序，a是数组，n表示数组大小
+func BubbleSort(a []int, n int) {
+	if n <= 1 {
+		return
+	}
+	for i := 0; i < n; i++ {
+		// 提前退出标志
+		flag := false
+		for j := 0; j < n-i-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+				//此次冒泡有数据交换
+				flag = true
+			}
+		}
+		// 如果没有交换数据，提前退出
+		if !flag {
+			break
+		}
+	}
+}
+
+// 插入排序，a表示数组，n表示数组大小
+func InsertionSort(a []int, n int) {
+	if n <= 1 {
+		return
+	}
+	for i := 1; i < n; i++ {
+		value := a[i]
+		j := i - 1
+		//查找要插入的位置并移动数据
+		for ; j >= 0; j-- {
+			if a[j] > value {
+				a[j+1] = a[j]
+			} else {
+				break
+			}
+		}
+		a[j+1] = value
+	}
+}
+
+// 选择排序，a表示数组，n表示数组大小
+func SelectionSort(a []int, n int) {
+	if n <= 1 {
+		return
+	}
+	for i := 0; i < n; i++ {
+		// 查找最小值
+		minIndex := i
+		for j := i + 1; j < n; j++ {
+			if a[j] < a[minIndex] {
+				minIndex = j
+			}
+		}
+		// 交换
+		a[i], a[minIndex] = a[minIndex], a[i]
+
+	}
+}
+func main() {
+	nums := []int{2, 4, 2, 5, 762, 141}
+	insertionSort(nums, len(nums))
+	fmt.Println(nums)
+}
