66887504e304e213a60689d142c9df669b25e61e
shr26207 <sunhaoran@offcn.com>
2022-09-19 18:09:49 +0800

    2022年9月19日

---
+++ algorithm/sort/p215/findKthLargest.go
@@ -0,0 +1,34 @@
+package p215
+
+func findKthLargest(nums []int, k int) int {
+	l, r := 0, len(nums)-1
+	k = len(nums) - 1 - k
+	for l <= r {
+		p := Partition(nums, l, r)
+		if p < k {
+			// 第 k 大的元素在 nums[p+1..hi] 中
+			l = p + 1
+		} else if p > k {
+			// 第 k 大的元素在 nums[lo..p-1] 中
+			r = p - 1
+		} else {
+			// 找到第 k 大元素
+			return nums[p]
+		}
+	}
+	return -1
+}
+
+func Partition(nums []int, low, height int) int {
+	i, pivot := low, nums[height]
+	for j := low; j < height; j++ {
+		if nums[j] < pivot {
+			if i != j {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+			i++
+		}
+	}
+	nums[i], nums[height] = nums[height], nums[i]
+	return i
+}
