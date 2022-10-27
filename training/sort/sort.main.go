65131c2dea68faf68e9672e1c398c5b61a11d725
shr26207 <sunhaoran@offcn.com>
2022-10-27 09:16:56 +0800

    2022年10月24日

--- training/sort/sort.main.go
+++ training/sort/sort.main.go
@@ -35,7 +35,7 @@ func partition(nums []int, lo, he int) int {
 		}
 		nums[i], nums[j] = nums[j], nums[i]
 	}
-	nums[j], nums[lo] = nums[lo], nums[j]
+	nums[lo], nums[j] = nums[j], nums[lo]
 	return j
 }
 
