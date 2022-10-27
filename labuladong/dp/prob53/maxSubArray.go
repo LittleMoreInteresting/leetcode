70f64648e93e97e66b1c5ce417d8d9c228e61e4e
shr26207 <sunhaoran@offcn.com>
2022-10-27 13:28:19 +0800

    2022年10月27日

--- labuladong/dp/prob53/maxSubArray.go
+++ labuladong/dp/prob53/maxSubArray.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	"leetcode/labuladong/dp/public"
 )
 
 func main() {
-	fmt.Println(maxSubArray([]int{-2,1,-3,4,-1,2,1,-5,4}))
-	fmt.Println(maxSubArray00([]int{-2,1,-3,4,-1,2,1,-5,4}))
+	fmt.Println(maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
+	fmt.Println(maxSubArray00([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
+	fmt.Println(maxSubArrayWin([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
 }
 
 func maxSubArray00(nums []int) int {
@@ -15,7 +17,7 @@ func maxSubArray00(nums []int) int {
 	dp := make([]int, l) //定义：dp[i] 记录以 nums[i] 为结尾的「最大子数组和」
 	dp[0] = nums[0]
 	for i := 1; i < l; i++ {
-		dp[i] = public.Max(nums[i],nums[i]+dp[i-1])
+		dp[i] = public.Max(nums[i], nums[i]+dp[i-1])
 	}
 	return public.Max(dp...)
 }
@@ -23,11 +25,30 @@ func maxSubArray00(nums []int) int {
 func maxSubArray(nums []int) int {
 	l := len(nums)
 	dp_0 := nums[0]
-	dp,res := 0,dp_0
+	dp, res := 0, dp_0
 	for i := 1; i < l; i++ {
-		dp = public.Max(nums[i],nums[i]+dp_0)
+		dp = public.Max(nums[i], nums[i]+dp_0)
 		dp_0 = dp
-		res = public.Max(res,dp)
+		res = public.Max(res, dp)
+	}
+	return res
+}
+
+//滑动窗口
+func maxSubArrayWin(nums []int) int {
+	left, right := 0, 0
+	winMax := 0
+	res := nums[0]
+	for right < len(nums) {
+		winMax += nums[right]
+		right++
+		if winMax > res {
+			res = winMax
+		}
+		for winMax < 0 {
+			winMax -= nums[left]
+			left++
+		}
 	}
 	return res
 }
