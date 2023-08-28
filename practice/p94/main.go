05c7c570969f396f1fcbad97331fc91b1184f640
shr26207 <sunhaoran@offcn.com>
2023-08-28 15:09:47 +0800

    2023.08.21

---
+++ practice/p94/main.go
@@ -0,0 +1,26 @@
+package p94
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	res := []int{}
+	inorder(root, &res)
+	return res
+}
+
+func inorder(root *TreeNode, res *[]int) {
+	if root == nil {
+		return
+	}
+	if root.Left != nil {
+		inorder(root.Left, res)
+	}
+	*res = append(*res, root.Val)
+	if root.Right != nil {
+		inorder(root.Right, res)
+	}
+}
---
+++ practice/p98/main.go
@@ -0,0 +1,32 @@
+package p98
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isValidBST(root *TreeNode) bool {
+	return isBST(root, nil, nil)
+}
+
+func isBST(root, min, max *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if min != nil && root.Val <= min.Val {
+		return false
+	}
+	if max != nil && root.Val >= max.Val {
+		return false
+	}
+	return isBST(root.Left, min, root) && isBST(root.Right, root, max)
+}
